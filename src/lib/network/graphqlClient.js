a74649182243f8acd68a8314d543039632ec9401
big-javascript-fan <wdeveloping9099@gmail.com>
2022-05-12 22:12:33 -0400

    update url endpoint with static value

--- src/lib/network/graphqlClient.js
+++ src/lib/network/graphqlClient.js
@@ -2,13 +2,15 @@ import { GraphQLClient } from 'graphql-request'
 
 const environment = process.env.NEXT_PUBLIC_ENV || process.env.NEXT_PUBLIC_LOCAL || 'local'
 
+const localApiEndpoint = 'http://localhost:4444/graphql'
+
 const endpoint =
   environment === 'production'
     ? process.env.NEXT_PUBLIC_PROD_API
     : environment === 'staging'
     ? process.env.NEXT_PUBLIC_STAGE_API
     : environment === 'local'
-    ? process.env.NEXT_PUBLIC_LOCAL_API
+    ? localApiEndpoint
     : process.env.NEXT_PUBLIC_DEV_API
 
 console.log('API endpoint:', endpoint)
