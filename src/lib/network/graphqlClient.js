d63acac427cc61419d2c64f8bb1951acd3b2a280
big-javascript-fan <wdeveloping9099@gmail.com>
2022-05-15 21:54:39 -0400

    remove env and update api endpoint

--- src/lib/network/graphqlClient.js
+++ src/lib/network/graphqlClient.js
@@ -2,16 +2,16 @@ import { GraphQLClient } from 'graphql-request'
 
 const environment = process.env.NEXT_PUBLIC_ENV || process.env.NEXT_PUBLIC_LOCAL || 'local'
 
-const devApiEndpoint = 'http://api-marketplace.neocosmos.io/graphql'
+const endpoint = 'http://api-marketplace.neocosmos.io/graphql'
 
-const endpoint =
-  environment === 'production'
-    ? process.env.NEXT_PUBLIC_PROD_API
-    : environment === 'staging'
-    ? process.env.NEXT_PUBLIC_STAGE_API
-    : environment === 'local'
-    ? devApiEndpoint
-    : process.env.NEXT_PUBLIC_DEV_API
+// const endpoint =
+//   environment === 'production'
+//     ? process.env.NEXT_PUBLIC_PROD_API
+//     : environment === 'staging'
+//     ? process.env.NEXT_PUBLIC_STAGE_API
+//     : environment === 'local'
+//     ? process.env.REACT_APP_API_HOST
+//     : process.env.NEXT_PUBLIC_DEV_API
 
 console.log('API endpoint:', endpoint)
 
